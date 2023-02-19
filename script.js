edb84a7b8962025b79df1828dc3e20c30acc915c
Anton Roters <antonroters@hotmail.com>
2023-02-19 16:22:26 +0100

    setup default layer for navbar

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const progressBar = document.querySelector('[data-js="progress-bar"]');
+const navbar = document.querySelector('[data-js="navbar"]');
+const navbarToggle = document.querySelector('[data-js="navbar-toggle"]');
+
+navbarToggle.addEventListener("click", () => {
+  navbar.classList.toggle("responsive");
+});
 
 document.addEventListener("scroll", () => {
   progressBar.style.width = `${getScrollProgress()}%`;
 });
 
 function getScrollProgress() {
-  return (window.scrollY / document.body.clientHeight) * 100;
+  return (
+    (window.scrollY / (document.body.clientHeight + window.innerHeight)) * 100
+  );
 }
